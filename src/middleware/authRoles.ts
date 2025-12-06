282194f52e7acff08acd1693eb1d81803b6979d2
M0hamed-Gamall <jimmy.mans00ur@gmail.com>
2025-12-06 20:44:45 +0200

    create authRoles

---
+++ src/middleware/authRoles.ts
@@ -0,0 +1,28 @@
+import type { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import AppError from '../utils/appError.js';
+
+export const authRoles = (...roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (token) {
+      try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+        const userRole = (decoded as any).role ;
+        console.log(userRole)
+        if (userRole && roles.includes(userRole)) {
+          (req as any).user = decoded;
+          return next();
+        } 
+      } catch (err){
+        if ((err as any).name === "TokenExpiredError") {
+          throw new AppError("Access token expired", 401, "TokenExpired");
+        }
+        throw new AppError("access denied, invalid token", 401, 'Unauthorized')
+      }
+    } else{
+      throw new AppError("access denied, no token provided", 401, 'Unauthorized');
+    } 
+    throw new AppError("you are unauthorized", 401, 'Unauthorized');
+  }
+}
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -3,6 +3,7 @@ import authController from '../controllers/auth.controller.js';
 import { validate } from '../middleware/validate.js';
 import { registerValidator } from '../validators/registerValidator.js';
 import { loginValidator } from '../validators/loginValidator.js';
+import { authRoles } from '../middleware/authRoles.js';
 
 const router = Router();
 
@@ -10,5 +11,9 @@ const router = Router();
 router.post('/register', registerValidator, validate, authController.register);
 router.post('/login', loginValidator, validate, authController.login);
 router.post('/logout', authController.logout);
+
+router.get('/protected', authRoles("admin"), (req,res,next) => {
+  res.send("passed")
+})
 
 export default router;
